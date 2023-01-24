b9dab1823b56f303fe4930123c12dd1c69d66446
hkwonx12 <helenkwonx2@gmail.com>
2023-01-23 20:19:29 -0500

    Feature 15 complete

---
+++ tasks/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Task
+
+class CreateTaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields =[
+            "name",
+            "start_date",
+            "due_date",
+            "project",
+            "assignee",
+        ]
